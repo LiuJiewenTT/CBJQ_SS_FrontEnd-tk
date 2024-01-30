58cdc62b9f3c7bfa463e5c653cf67b0bb457fbca
刘杰文 <1635370493@qq.com>
2024-01-30 19:41:30 +0800

    添加日志窗；自动滚动和自动打印功能已就绪。

--- src/CBJQ_SS_FrontEnd_tk/main.py
+++ src/CBJQ_SS_FrontEnd_tk/main.py
@@ -5,6 +5,8 @@ import shutil
 import os.path as osp
 import os
 import sys
+from typing import Dict
+from functools import partial
 
 tkinter_NWES = (tkinter.N, tkinter.W, tkinter.E, tkinter.S)
 
@@ -19,17 +21,45 @@ def checkExecutableReadiness(exec_name: str):
     return True
 
 
+def printAndInsertWrapper(func):
+    def func2(*args, **kwargs):
+        print_flag = kwargs.get('print')
+        if print_flag is not False:
+            endding = kwargs.get('end')
+            if print_flag is not None:  # print=True
+                kwargs.pop('print')
+            print(args[1], end=endding)
+        func.__call__(*args, **kwargs)
+    return func2
+
+
+@printAndInsertWrapper
+def insertAndScrollToEnd(self, arg2, end='\n'):
+    self.insert('end', str(arg2)+end)
+    self.yview_moveto(1)
+
+
 class CBJQ_SS_FrontEnd_tk:
 
-    server_list: dict
+    server_list: Dict[str, str]
     backend_path: str
+    displayLog_frame_state: bool
 
     def __init__(self, **kwargs):
+        """初始化
+
+        :param kwargs:  backend_path: str,
+                        server_list: Dict[str, str]
+        """
         self.backend_path = kwargs.get('backend_path')
         self.server_list = kwargs.get('server_list')
 
+        self.displayLog_frame_state = False
+
         self.root_window = tkinter.Tk()
         self.root_window.title("尘白禁区服务器切换器 - 前端")
+        self.root_window.rowconfigure(0, weight=1)
+        self.root_window.columnconfigure(0, weight=1)
 
         # Define Section
         # 命名规则：实例名称+类名
@@ -40,14 +70,16 @@ class CBJQ_SS_FrontEnd_tk:
         # Define main_frame
         self.main_frame = ttk.Frame(self.root_window, padding=(3, 12),
                                width=400, height=300, style='main_frame.TFrame')
-        self.main_frame.grid(sticky=tkinter_NWES)
+        # Define listServer_frame
+        self.listServer_frame = ttk.Frame(self.main_frame, padding=(5, 5))
+        self.listServer_frame.rowconfigure(1, weight=1)
         # Define listServer_label
         self.listServer_label_Var = tkinter.StringVar(value='预设服务器列表：')
-        self.listServer_label = ttk.Label(self.main_frame, textvariable=self.listServer_label_Var)
+        self.listServer_label = ttk.Label(self.listServer_frame, textvariable=self.listServer_label_Var)
         # Define listServer_listbox
         self.listServer_listbox_choice = list(self.server_list.keys())
         self.listServer_listbox_choice_Var = tkinter.StringVar(value=self.listServer_listbox_choice)
-        self.listServer_listbox = tkinter.Listbox(self.main_frame, listvariable=self.listServer_listbox_choice_Var, width=20)
+        self.listServer_listbox = tkinter.Listbox(self.listServer_frame, listvariable=self.listServer_listbox_choice_Var, width=20)
         # Define doAction_frame
         self.doAction_frame = ttk.Frame(self.main_frame, padding=(5, 5))
         # Define doSwitch_button
@@ -58,41 +90,103 @@ class CBJQ_SS_FrontEnd_tk:
         self.doSwitchAndRun_button_Var = tkinter.StringVar(value='切换并启动')
         self.doSwitchAndRun_button = ttk.Button(self.doAction_frame, textvariable=self.doSwitchAndRun_button_Var,
                                                 command=lambda: self.execAction(action='s&r'))
+        # Define toggleLogDisplay_button
+        self.toggleLogDisplay_button_Var = tkinter.StringVar(value='日志 >')
+        self.toggleLogDisplay_button = ttk.Button(self.doAction_frame, textvariable=self.toggleLogDisplay_button_Var,
+                                                  command=self.toggleLogDisplay)
+        # Define displayLog_frame
+        self.displayLog_frame = ttk.Frame(self.main_frame, padding=(5, 5))
+        # Define displayLog_titlebar_frame
+        self.displayLog_titlebar_frame = ttk.Frame(self.displayLog_frame)
+        # Define displayLog_label
+        self.displayLog_label_Var = tkinter.StringVar(value='日志输出：')
+        self.displayLog_label = ttk.Label(self.displayLog_titlebar_frame, textvariable=self.displayLog_label_Var, anchor=tkinter.W)
+        # Define displayLog_clean_button
+        self.displayLog_clean_button_Var = tkinter.StringVar(value='清空')
+        self.displayLog_clean_button = ttk.Button(self.displayLog_titlebar_frame,
+                                                  textvariable=self.displayLog_clean_button_Var,
+                                                  command=self.cleanLogDisplayed)
+        # Define displayLog_text
+        self.displayLog_text = tkinter.Text(self.displayLog_frame, width=100, height=30)
+        # self.displayLog_text['state'] = 'normal'
+        # self.displayLog_text.bind('<Control C>', lambda e: self.root_window.clipboard_append(e), print('copied'))
+        self.displayLog_text.bind('<Key>', lambda e: (self.root_window.clipboard_append(e)
+                                                      if e.state == 12 and e.keysym == 'c' else "break"))  # Read Only
+        self.displayLog_text.insertAndScrollToEnd = partial(insertAndScrollToEnd, self.displayLog_text) # instance method
+        # Define displayLog_label_scrollbar
+        self.displayLog_text_scrollbar = ttk.Scrollbar(self.displayLog_frame, orient=tkinter.VERTICAL,
+                                                       command=self.displayLog_text.yview)
+        self.displayLog_text['yscrollcommand'] = self.displayLog_text_scrollbar.set    # 设置双方回调以实现交流
+        # Define statusbar
+        self.statusbar_label_Var = tkinter.StringVar(value='作者: LiuJiewenTT <liuljwtt@163.com>。')
+        self.statusbar_label = ttk.Label(self.root_window, textvariable=self.statusbar_label_Var, anchor=tkinter.W)
 
     def run(self):
         # Geometry Management
-        self.listServer_label.grid(column=0, row=0, sticky=tkinter.W)
-        self.listServer_listbox.grid(column=0, row=1)
+        self.main_frame.grid(sticky=tkinter_NWES)
+        self.listServer_frame.grid(column=0, row=0, sticky=tkinter_NWES)
+        self.listServer_label.grid(column=0, row=0, sticky=(tkinter.W, tkinter.S))
+        self.listServer_listbox.grid(column=0, row=1, sticky=tkinter_NWES)
+        self.doAction_frame.grid(column=1, row=0, rowspan=1, sticky=tkinter.N)
         self.doSwitch_button.grid(column=0, row=0)
         self.doSwitchAndRun_button.grid(column=0, row=1)
-        self.doAction_frame.grid(column=1, row=0, rowspan=2, sticky=tkinter.N)
+        self.toggleLogDisplay_button.grid(column=0, row=2, sticky=tkinter.S)
+        self.displayLog_titlebar_frame.grid(column=0, row=0, columnspan=2, sticky=tkinter_NWES)
+        self.displayLog_label.grid(column=0, row=0, sticky=(tkinter.W, tkinter.S))
+        self.displayLog_clean_button.grid(column=1, row=0, sticky=(tkinter.E, tkinter.S))
+        self.displayLog_text.grid(column=0, row=1, sticky=tkinter.N)
+        self.displayLog_text_scrollbar.grid(column=1, row=1, sticky=(tkinter.N, tkinter.S))
+        self.displayLog_clean_button.grid(column=1, row=0, sticky=(tkinter.E, tkinter.S))
+        self.statusbar_label.grid(column=0, row=1, columnspan=3, sticky=(tkinter.W, tkinter.S))
         self.root_window.mainloop()
         return
 
+    def toggleLogDisplay(self):
+        if self.displayLog_frame_state:
+            self.displayLog_frame_state = False
+            self.displayLog_frame.grid_forget()
+            self.toggleLogDisplay_button_Var.set('日志 >')
+        else:
+            self.displayLog_frame_state = True
+            self.displayLog_frame.grid(column=2, row=0, rowspan=1, sticky=tkinter_NWES)
+            self.toggleLogDisplay_button_Var.set('日志 <')
+        return
+
+    def cleanLogDisplayed(self):
+        self.displayLog_text.delete('1.0', tkinter.END)
+
     def execAction(self, **kwargs):
         print(kwargs)
+        self.displayLog_text.insertAndScrollToEnd(kwargs)
         launch_args = []
         if kwargs.get('action') in ['s', 's&r']:
             print('do switch')
+            self.displayLog_text.insertAndScrollToEnd('do switch')
             launch_args.append('-nopause')
             if kwargs['action'] == 's&r':
                 print('launch')
+                self.displayLog_text.insertAndScrollToEnd('launch')
             else:
                 launch_args.append('-nostart')
             curselection_idxs: tuple = self.listServer_listbox.curselection()
-            print(curselection_idxs)
+            print(f'curselection: {curselection_idxs}')
+            self.displayLog_text.insertAndScrollToEnd(f'curselection: {curselection_idxs}')
             if len(curselection_idxs) > 0:
                 curselection_idx = int(curselection_idxs[0])
                 curselection_value = self.server_list[self.listServer_listbox_choice[curselection_idx]]
                 launch_args.append(curselection_value)
             launch_cmd = [self.backend_path]
             launch_cmd.extend(launch_args)
-            print(launch_cmd)
+            print(f'launch_cmd: {launch_cmd}')
+
             with subprocess.Popen(launch_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8') as sp:
-                print(sp.pid)
+                print(f'sp.pid: {sp.pid}')
                 output = sp.stdout.readlines()
                 for i in output:
                     print(f'[stdout]: {i}', end='')
+                    # self.displayLog_text.insert('end', i)
+                    # self.displayLog_text.yview_moveto(1)
+                    self.displayLog_text.insertAndScrollToEnd(i, end='')     # instance method
                 print('')
         pass
 
